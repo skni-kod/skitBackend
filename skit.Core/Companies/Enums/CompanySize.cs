0e64e4cb38c1c7bd6687783008212588cb76c098
Comply5000 <wojciech.kaczor.comply@gmail.com>
2023-10-25 22:56:24 +0200

    #1 - fix

--- skit.Core/Companies/Enums/CompanySize.cs
+++ skit.Core/Companies/Enums/CompanySize.cs
@@ -1,9 +1,14 @@
-﻿namespace skit.Core.Companies.Enums;
+﻿using System.ComponentModel;
+
+namespace skit.Core.Companies.Enums;
 
 public enum CompanySize
 {
     NoData = 0,
+    [Description("1-50")]
     Small = 1,
+    [Description("50-300")]
     Medium = 2,
+    [Description("300+")]
     Large = 3
 }
--- skit.Core/Offers/Enums/OfferStatus.cs
+++ skit.Core/Offers/Enums/OfferStatus.cs
@@ -5,4 +5,5 @@ public enum OfferStatus
     NoData = 0,
     Draft = 1,
     Public = 2,
+    Finished = 3
 }
