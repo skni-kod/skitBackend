bbf9d2b0130d5e651398fbc55b52f9e08bbfb83f
Mikołaj Kuszowski <mikolaj.kuszowski@cetuspro.com>
2023-10-31 00:46:46 +0100

    Company repository updated

--- skit.Infrastructure/DAL/Companies/Repositories/CompanyRepository.cs
+++ skit.Infrastructure/DAL/Companies/Repositories/CompanyRepository.cs
@@ -8,19 +8,21 @@ namespace skit.Infrastructure.DAL.Companies.Repositories;
 internal sealed class CompanyRepository : ICompanyRepository
 {
     private readonly EFContext _context;
+    private readonly DbSet<Company> _companies;
 
     public CompanyRepository(EFContext context)
     {
         _context = context;
+        _companies = _context.Companies;
     }
 
     public async Task<Company> GetAsync(Guid companyId, CancellationToken cancellationToken)
-        => await _context.Companies.SingleOrDefaultAsync(company => company.Id == companyId, cancellationToken: cancellationToken);
+        => await _companies.SingleOrDefaultAsync(company => company.Id == companyId, cancellationToken: cancellationToken);
 
 
     public async Task UpdateAsync(Company company, CancellationToken cancellationToken)
     {
-        _context.Companies.Update(company);
+        _companies.Update(company);
         await _context.SaveChangesAsync(cancellationToken);
     }
 }
