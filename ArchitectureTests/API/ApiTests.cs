2696a6e823d68f3bd57fa1f82bb5ae6a19560e6a
Kacper Maciołek <kacper.maciolek00@gmail.com>
2023-11-04 16:52:28 +0100

    #11 - architecture tests added

---
+++ ArchitectureTests/API/ApiTests.cs
@@ -0,0 +1,26 @@
+﻿using skit.API;
+using System.Reflection;
+
+namespace ArchitectureTests.API;
+public class ApiTests
+{
+    private static readonly Assembly ApiAssembly = typeof(AssemblyReference).Assembly;
+
+    [Fact]
+    public void Controllers_Should_HaveDependencyOnMediatR()
+    {
+        // Arrange
+
+        // Act
+        var result = Types
+            .InAssembly(ApiAssembly)
+            .That()
+            .HaveNameEndingWith("Controller")
+            .Should()
+            .HaveDependencyOn("MediatR")
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
---
+++ ArchitectureTests/Application/ApplicationTests.cs
@@ -0,0 +1,28 @@
+﻿using skit.Application;
+using System.Reflection;
+
+namespace ArchitectureTests.Application;
+public class ApplicationTests
+{
+    private const string CoreNamespace = "skit.Core";
+
+    private static readonly Assembly ApplicationAssembly = typeof(AssemblyReference).Assembly;
+
+    [Fact]
+    public void Handlers_Should_HaveDependencyOnCore()
+    {
+        // Arrange
+
+        // Act
+        var result = Types
+            .InAssembly(ApplicationAssembly)
+            .That()
+            .HaveNameEndingWith("Handler")
+            .Should()
+            .HaveDependencyOn(CoreNamespace)
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
