fbff895f5b88cc47b7adaffcc5f13aa3cff087b8
Mikołaj Kuszowski <mikolaj.kuszowski@cetuspro.com>
2023-11-10 13:35:34 +0100

    Private setters for offer and salary tables

--- skit.Core/Offers/Entities/Offer.cs
+++ skit.Core/Offers/Entities/Offer.cs
@@ -10,18 +10,57 @@ namespace skit.Core.Offers.Entities;
 
 public sealed class Offer : Entity
 {
-    public string Title { get; set; }
-    public string? Description { get; set; }
-    public DateTimeOffset? DateFrom { get; set; }
-    public DateTimeOffset? DateTo { get; set; }
-    public OfferStatus Status { get; set; }
-    public OfferSeniority Seniority { get; set; }
-    public OfferWorkLocation WorkLocation { get; set; }
+    public string Title { get; private set; }
+    public string? Description { get; private set; }
+    public DateTimeOffset? DateFrom { get; private set; }
+    public DateTimeOffset? DateTo { get; private set; }
+    public OfferStatus Status { get; private set; }
+    public OfferSeniority Seniority { get; private set; }
+    public OfferWorkLocation WorkLocation { get; private set; }
     
-    public Guid CompanyId { get; set; }
-    public Company Company { get; set; }
+    public Guid CompanyId { get; private set; }
+    public Company Company { get; private set; }
     
-    public List<Address> Addresses { get; set; }
-    public List<Salary> Salaries { get; set; }
-    public List<JobApplication> JobApplications { get; set; }
+    private List<Address> _addresses => new();
+    public IReadOnlyCollection<Address> Addresses => _addresses;
+    private List<Salary> _salaries => new();
+    public IReadOnlyCollection<Salary> Salaries => _salaries;
+    private List<JobApplication> _jobApplications => new();
+    public IReadOnlyCollection<JobApplication> JobApplications => _jobApplications;
+
+    private Offer()
+    {
+    }
+
+    private Offer(
+        string title, 
+        string? description, 
+        DateTimeOffset? dateFrom, 
+        DateTimeOffset? dateTo, 
+        OfferStatus status,
+        OfferSeniority seniority,
+        OfferWorkLocation workLocation,
+        Guid companyId
+    )
+    {
+        Title = title;
+        Description = description;
+        DateFrom = dateFrom;
+        DateTo = dateTo;
+        Status = status;
+        Seniority = seniority;
+        WorkLocation = workLocation;
+        CompanyId = companyId;
+    }
+
+    public static Offer Create(
+        string title,
+        string? description,
+        DateTimeOffset? dateFrom,
+        DateTimeOffset? dateTo,
+        OfferStatus status,
+        OfferSeniority seniority,
+        OfferWorkLocation workLocation,
+        Guid companyId) =>
+        new(title, description, dateFrom, dateTo, status, seniority, workLocation, companyId);
 }
