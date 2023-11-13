4c09479c1c1773ec3a64d019abf6768195111e4f
Comply5000 <wojciech.kaczor.comply@gmail.com>
2023-11-13 16:37:52 +0100

    #4 - BrowseAddresses

---
+++ skit.Application/Addresses/Queries/BrowseAddresses/BrowseAddressesQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace skit.Application.Addresses.Queries.BrowseAddresses;
+
+public sealed record BrowseAddressesQuery(string? Search) : IRequest<BrowseAddressesResponse>;
---
+++ skit.Application/Addresses/Queries/GetAddress/GetAddressQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace skit.Application.Addresses.Queries.GetAddress;
+
+public sealed record GetAddressQuery(Guid Id) : IRequest<GetAddressForUpdate.GetAddressResponse>;
