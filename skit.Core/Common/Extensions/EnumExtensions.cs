ca5c3384e3f876c33b8186eb7506ca0d89f6872a
Comply5000 <wojciech.kaczor.comply@gmail.com>
2023-11-27 16:24:12 +0100

    #22 - added enums controller, added enum extensions

---
+++ skit.Core/Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,67 @@
+﻿using skit.Core.Common.DTO;
+
+namespace skit.Core.Common.Extensions;
+
+public static class EnumExtensions
+{
+    public static List<BaseEnum> GetValues<TEnum>() where TEnum : Enum
+    {
+        return Enum.GetValues(typeof(TEnum))
+            .Cast<TEnum>()
+            .Where(x => !x.Equals(default(TEnum)))
+            .Select(x => new BaseEnum {Id = Convert.ToInt32(x), Name = x.ToString()})
+            .ToList();
+    }
+    
+    public static List<BaseEnum> GetObjectsFromFlag<TEnum>(this TEnum flagsEnum) where TEnum : Enum
+    {
+        var result = new List<BaseEnum>();
+
+        foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
+        {
+            if (value.Equals(Enum.ToObject(typeof(TEnum), 0)))
+                continue;
+            
+            if (flagsEnum.HasFlag(value))
+            {
+                result.Add(value);
+            }
+        }
+        
+        return result;
+    }
+    
+    public static List<TEnum> GetValuesFromFlag<TEnum>(this TEnum flagsEnum) where TEnum : Enum
+    {
+        var result = new List<TEnum>();
+
+        foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
+        {
+            if (value.Equals(Enum.ToObject(typeof(TEnum), 0)))
+                continue;
+            
+            if (flagsEnum.HasFlag(value))
+            {
+                result.Add(value);
+            }
+        }
+        
+        return result;
+    }
+
+    public static TEnum AggregateToFlag<TEnum>(this List<TEnum>? enumValues) where TEnum : Enum
+    {
+        if (enumValues == null)
+            return (TEnum)Enum.ToObject(typeof(TEnum), 0);
+
+        var resultValue = 0;
+
+        foreach (var enumValue in enumValues)
+        {
+            resultValue |= Convert.ToInt32(enumValue);
+        }
+
+        var result = (TEnum)Enum.ToObject(typeof(TEnum), resultValue);
+        return result;
+    }
+}
