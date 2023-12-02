673516b5a9fc4a7ea11f31476aee33346be2fcc1
Mikołaj Kuszowski <mikolaj.kuszowski@cetuspro.com>
2023-12-02 17:48:29 +0100

    main merge

--- skit.API/Controllers/Areas/Auth/AccountController.cs
+++ skit.API/Controllers/Areas/Auth/AccountController.cs
@@ -11,7 +11,7 @@ public sealed class AccountController : BaseController
     [HttpPost("sign-up-company")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<JwtWebToken>> SignUpCompany([FromBody] SignUpCompanyCommand command,
+    public async Task<ActionResult<JsonWebToken>> SignUpCompany([FromBody] SignUpCompanyCommand command,
         CancellationToken cancellationToken)
     {
         var result = await Mediator.Send(command, cancellationToken);
@@ -21,7 +21,7 @@ public sealed class AccountController : BaseController
     [HttpPost("sign-in")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<JwtWebToken>> SignIn([FromBody] SignInCommand command, CancellationToken cancellationToken)
+    public async Task<ActionResult<JsonWebToken>> SignIn([FromBody] SignInCommand command, CancellationToken cancellationToken)
     {
         var result = await Mediator.Send(command, cancellationToken);
         return Ok(result);
