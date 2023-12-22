0d7e517badc5025c88ba9a382abf01506f3419ff
Mikołaj Kuszowski <mikolaj.kuszowski@cetuspro.com>
2023-12-22 01:43:53 +0100

    Query filter model edited

--- skit.Application/Offers/Queries/BrowsePublicOffers/BrowsePublicOffersQuery.cs
+++ skit.Application/Offers/Queries/BrowsePublicOffers/BrowsePublicOffersQuery.cs
@@ -1,6 +1,15 @@
 ﻿using MediatR;
+using skit.Core.Offers.Enums;
+using skit.Core.Salaries.Enums;
 using skit.Shared.Models;
 
 namespace skit.Application.Offers.Queries.BrowsePublicOffers;
 
-public sealed record BrowsePublicOffersQuery(string? Search) : PaginationRequest, IRequest<BrowsePublicOffersResponse>;
+public sealed record BrowsePublicOffersQuery(
+    string? Search,
+    List<OfferSeniority> OfferSeniorities,
+    List<OfferWorkLocation> OfferWorkLocations,
+    List<string> Cities,
+    decimal SalaryFrom,
+    List<SalaryEmploymentType> EmploymentType,
+    List<Guid> TechnologyIds) : PaginationRequest, IRequest<BrowsePublicOffersResponse>;
