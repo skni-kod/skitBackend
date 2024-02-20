d25678016b59e1770e228d0f08e685a1468bc63b
Mikołaj Kuszowski <mikolajkuszowski@gmail.com>
2024-02-20 12:45:53 +0100

    GetJobApplication logic added

---
+++ skit.Infrastructure/DAL/JobApplications/Queries/Extentions.cs
@@ -0,0 +1,17 @@
+﻿using skit.Application.JobApplications.Queries.DTO;
+using skit.Core.JobApplications.Entities;
+
+namespace skit.Infrastructure.DAL.JobApplications.Queries;
+
+internal static class Extensions
+{
+    public static JobApplicationDto AsDetailsDto(this JobApplication jobApplication)
+    {
+        return new JobApplicationDto
+        {
+            FirstName = jobApplication.FirstName,
+            SurName = jobApplication.SurName,
+            Description = jobApplication.Description
+        };
+    }
+}
---
+++ skit.Infrastructure/DAL/JobApplications/Queries/GetJobApplication/GetJobApplicationHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using skit.Application.JobApplications.Queries.GetJobApplication;
+using skit.Core.Common.Services;
+using skit.Infrastructure.DAL.EF.Context;
+
+namespace skit.Infrastructure.DAL.JobApplications.Queries.GetJobApplication;
+
+internal sealed class GetJobApplicationHandler : IRequestHandler<GetJobApplicationQuery, GetJobApplicationResponse?>
+{
+    private readonly EFContext _context;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetJobApplicationHandler(EFContext context, ICurrentUserService currentUserService)
+    {
+        _context = context;
+        _currentUserService = currentUserService;
+    }
+
+    public async Task<GetJobApplicationResponse?> Handle(GetJobApplicationQuery request, CancellationToken cancellationToken)
+    {
+        var jobApplication = await _context.JobApplications.AsNoTracking()
+            .Include(jobApplication => jobApplication.Offer)
+            .Where(jobApplication => jobApplication.Offer.CompanyId == _currentUserService.CompanyId)
+            .Where(jobApplication => jobApplication.Id == request.JobApplicationId)
+            .Select(jobApplication => new GetJobApplicationResponse(jobApplication.AsDetailsDto()))
+            .SingleOrDefaultAsync(cancellationToken);
+
+        return jobApplication;
+    }
+}
